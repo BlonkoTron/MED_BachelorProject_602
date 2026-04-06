b7504cba7f37d15c6cb487c663b0fc447180e5a9
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2026-04-06 13:34:32 +0200

    Eventmanager

---
+++ Assets/Scripts/Event_Manager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Event_Manager : MonoBehaviour
+{
+    //Eventinfo text
+    public Text Eventinfo;
+
+    //Stores eventdata
+    private Events selectedEvent;
+
+
+    public bool test;
+
+    //Enable/disable Panel
+    public GameObject Panel;
+
+    //List of events
+    public List<Events> InGameEvents;
+
+    //Testint
+    public int Testmoney;
+
+    [System.Serializable]
+    public class Events
+    {
+        public string Info;
+        public int Moneyloss;
+        public int Moneygain;
+        public bool Tiledestuction;
+        public bool TileSpawn;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (test)
+        {
+            //picks random event and display evnettext
+            selectedEvent = InGameEvents[Random.Range(0, InGameEvents.Count)];
+            Eventinfo.text = selectedEvent.Info;
+            test = false;
+        }
+    }
+
+    public void Buttonok()
+    {
+        //Remove money
+        Testmoney = Testmoney - selectedEvent.Moneyloss;
+        //Add money
+        Testmoney = Testmoney + selectedEvent.Moneygain;
+
+        if (selectedEvent.Tiledestuction)
+        {
+            //Destroy/replace tile here
+        }
+
+        if (selectedEvent.TileSpawn)
+        {
+            //Spawn tile here
+        }
+
+        //Remove panel
+        Panel.SetActive(false);
+    }
+}
