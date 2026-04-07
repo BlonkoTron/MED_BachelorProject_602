daf8aee07af97c84d88e5f5957fe292f8e849dc3
BlonkoTron <133660863+BlonkoTron@users.noreply.github.com>
2026-04-07 10:31:04 +0200

    Tile ui open and close initial stuff

--- Assets/Scripts/InteractionManager.cs
+++ Assets/Scripts/InteractionManager.cs
@@ -1,15 +1,33 @@
 using System.Threading;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using static UnityEngine.InputSystem.InputAction;
 
 public class InteractionManager : MonoBehaviour
 {
+    public InteractionManager Instance;
+
+    public bool openUI;
+
+    public UnityEvent closeUI;
 
     [SerializeField] private GameObject hoveredTile;
     [SerializeField] LayerMask interactionLayer;
     [SerializeField] float interactionDistance = 100f;
-    
+
+    private GameObject hitObj;
+
+    private GameObject lastClickedTile;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
     private void Update()
     {
         hoveredTile = CheckMouseHover();
@@ -17,20 +35,27 @@ public class InteractionManager : MonoBehaviour
 
     private GameObject CheckMouseHover()
     {
-        GameObject hitObj;
-
         Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
         if (Physics.Raycast(ray, out RaycastHit hit, interactionDistance,interactionLayer))
         {
             //Debug.Log(hit.collider.gameObject.name);
             hitObj = hit.collider.gameObject;
 
+            if (hitObj != null && !hitObj.GetComponent<TileChanger>().highlightObj.activeSelf)
+            {
+                hitObj.GetComponent<TileChanger>().highlightObj.SetActive(true);
+            }
+
             return hitObj;
 
         }
         else
         {
-            //Debug.Log("You Aint hitting shit");
+            if (hitObj != null && hitObj.GetComponent<TileChanger>().highlightObj.activeSelf)
+            {
+                hitObj.GetComponent<TileChanger>().highlightObj.SetActive(false);
+            }
+
             return null;
         }
 
@@ -45,19 +70,37 @@ public class InteractionManager : MonoBehaviour
     {
         if (action.performed)
         {
-            if (hoveredTile != null) 
-            { 
-                Debug.Log("Im Clickin on it: " +  hoveredTile.name);
+            if (!openUI)
+            {
+                if (hoveredTile != null)
+                {
+                    Debug.Log("Im Clickin on it: " + hoveredTile.name);
 
+                    hoveredTile.GetComponent<TileChanger>().OnClick();
 
+                    closeUI.AddListener(hoveredTile.GetComponent<TileChanger>().CloseUI);
+
+                    lastClickedTile = hoveredTile;
+                    openUI = true;
+
+                }
+            }
+            else
+            {
+                openUI = false;
+
+                closeUI.Invoke();
+
+                closeUI.RemoveListener(lastClickedTile.GetComponent<TileChanger>().CloseUI);
+
+                lastClickedTile = null;
 
             }
+
+
         }
 
 
     }
-
-
-
 
 }
