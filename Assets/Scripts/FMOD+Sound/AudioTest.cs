616f931351f870d7d42fae76fdcf8fb190589dce
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2026-04-07 11:15:51 +0200

    Big FMOD update

--- Assets/Scripts/FMOD+Sound/AudioTest.cs
+++ Assets/Scripts/FMOD+Sound/AudioTest.cs
@@ -7,7 +7,7 @@ using Mono.Cecil;
 public class AudioTest : MonoBehaviour
 {
     private EventInstance Soundtest;
-    [SerializeField] private EventReference tester;
+    //[SerializeField] private EventReference tester;
 
     private void Update()
     {
@@ -18,7 +18,7 @@ public class AudioTest : MonoBehaviour
     private void Start()
     {
         //The sound does not follow because it takes the position one time and does not follow
-        Soundtest = Audiomanager.instance.PlaySound(tester, transform.position);
+        //Soundtest = Audiomanager.instance.PlaySound(tester, transform.position);
     }
 
 }
