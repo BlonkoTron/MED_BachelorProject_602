7c2e2b0b03b7f2ce274d0fe9b401ff35e777d91a
oscarbagger <oscarbagger@live.dk>
2026-04-15 13:14:16 +0200

    game speed settings

---
+++ Assets/Scripts/GameSpeedSettings.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSpeedSettings : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    [SerializeField] private Button normalSpeedButton;
+    [SerializeField] private Button x2SpeedButton;
+    [SerializeField] private Button x3SpeedButton;
+    [SerializeField] private Button pauseButton;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameManager = GameManager.Instance;
+        gameManager.onGameStateChanged.AddListener(UpdateSpeedSettingButtons);
+        normalSpeedButton.interactable=false;
+    }
+
+    private void UpdateSpeedSettingButtons(GameManager.GameState state)
+    {
+        switch(state)
+        {
+            case GameManager.GameState.normal:
+                normalSpeedButton.interactable = false;
+                x2SpeedButton.interactable = true;
+                x3SpeedButton.interactable = true;
+                pauseButton.interactable = true;
+                break;
+            case GameManager.GameState.Paused:
+                normalSpeedButton.interactable = true;
+                x2SpeedButton.interactable = true;
+                x3SpeedButton.interactable = true;
+                pauseButton.interactable = false;
+                break;
+            case GameManager.GameState.Speedx2:
+                normalSpeedButton.interactable = true;
+                x2SpeedButton.interactable = false;
+                x3SpeedButton.interactable = true;
+                pauseButton.interactable = true;
+                break;
+            case GameManager.GameState.speedx3:
+                normalSpeedButton.interactable = true;
+                x2SpeedButton.interactable = true;
+                x3SpeedButton.interactable = false;
+                pauseButton.interactable = true;
+                break;
+        }
+    }
+    public void SetGameStateNormal()
+    {
+        gameManager.SetNewState(GameManager.GameState.normal);
+    }
+    public void SetGameStatePaused()
+    {
+        gameManager.SetNewState(GameManager.GameState.Paused);
+    }
+    public void SetGameState2XSpeed()
+    {
+        gameManager.SetNewState(GameManager.GameState.Speedx2);
+    }
+    public void SetGameState3XSpeed()
+    {
+        gameManager.SetNewState(GameManager.GameState.speedx3);
+    }
+
+}
