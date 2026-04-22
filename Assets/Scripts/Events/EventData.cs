e7e359bf712a60367ce411d45444ff2c2cad6f06
VHSTapeDVD <135975159+VHSTapeDVD@users.noreply.github.com>
2026-04-22 11:08:10 +0200

    Revert "Merge branch 'main' into AlexRestored"
    This reverts commit eef4609c7958e14d3fca09eb10c10a8ea7a94554, reversing
    changes made to 6dcc928db93455e813e7530b13486c0f38a60dd4.

--- Assets/Scripts/Events/EventData.cs
+++ Assets/Scripts/Events/EventData.cs
@@ -1,4 +1,3 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Events/Event Data")]
@@ -58,44 +57,19 @@ public class EventData : ScriptableObject
     private void ApplyChoice(ChoiceData choice)
     {
         Debug.Log("ApplyChoice called");
-
-        //Money calls
-        int ED_currentmoney = PointSystem.Instance.CurrentMoney;
         PointSystem.Instance.AddMoney(choice.moneyGain);
         PointSystem.Instance.LoseMoney(choice.moneyLoss);
-
-        if (PointSystem.Instance.CurrentMoney > ED_currentmoney)
-        {
-            // plus money
-        }
-        else if (PointSystem.Instance.CurrentMoney < ED_currentmoney)
-        {
-            // minus money
-        }
-        else
-        {
-            // no change
-        }
-
-        //Efficientmultiply calls
         PointSystem.Instance.mineEfficiencyMultiplier = choice.MineEfficiencymult;
         PointSystem.Instance.cowfieldEfficiencyMultiplier = choice.CowEfficiencymult;
         PointSystem.Instance.agroforestEfficiencyMultiplier = choice.AggroForestEfficiencymult;
         PointSystem.Instance.farmEfficiencyMultiplier = choice.FarmEfficiencymult;
-        PointSystem.Instance.UpdateEfficiencyUI();
-
-        //Happiness calls
         Happiness.Instance.DecreaseHappiness(choice.HappinessDown);
         Happiness.Instance.IncreaseHappiness(choice.HappinessUp);
-
-        //Start round
         GameManager.Instance.StartRound();
-
-
-        //Disable tile
         GameManager.Instance.gameSettings.DisableMineTilesGamesetting = choice.DisableMineTilesEvent;
         GameManager.Instance.gameSettings.DisableFarmTilesGamesetting = choice.DisableFarmTilesEvent;
         GameManager.Instance.gameSettings.DisableAgroTilesGamesetting = choice.DisableAgroTilesEvent;
         GameManager.Instance.gameSettings.DisableCowTilesGamesetting = choice.DisableCowTilesEvent;
+
     }
 }
